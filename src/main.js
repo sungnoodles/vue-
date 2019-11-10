17351f307f72e854dc9e3983926071c2464d4686
sungnoodles <2436007673@qq.com>
2019-11-10 15:48:03 +0800

    这是一个注释

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// 入口文件
+
+
+// 导入vue模块
+import Vue from 'vue'
+
+// 导入mui模块
+import './lib/mui/css/mui.min.css'
+
+// 按需导入mint-ul组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name,Header)
+
+//导入 App.vue组件
+import app from './App.vue'
+
+var vm = new Vue({
+  el:'#app',
+  render: c => c(app)
+})
